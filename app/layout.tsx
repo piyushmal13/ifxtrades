3d1baee632d831c2f0957a0c9d7bcdb330ce4aa0
Piyush Mal <piyushmal1301@gmail.com>
2026-02-19 08:40:02 +0530

    Fix layout metadata issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-"use client"
-
-import React from "react"
-
-export const dynamic = "force-dynamic"
-
-import './globals.css'
+import "./globals.css"
 import { AuthProvider } from "@/lib/auth-provider"
 import Header from "@/components/Header"
 import MarketTicker from "@/components/MarketTicker"
@@ -24,20 +18,12 @@ export default function RootLayout({
       <body>
 
         <AuthProvider>
-
-          {/* TOP TICKER */}
           <MarketTicker />
-
-          {/* HEADER */}
           <Header />
-
-          {/* MAIN CONTENT */}
           {children}
-
         </AuthProvider>
 
       </body>
     </html>
   )
 }
-
